95affd846d8961dcc317868417b2fcccc6fa729e
iris_ <iris_@SUNHEE_COM>
2023-03-24 22:19:01 +0900

    콘솔창을 이용한 출력과 입력

---
+++ src/main/kotlin/Hello.kt
@@ -0,0 +1,13 @@
+fun main(args : Array<String>){
+    println("Hello!")
+    println("안녕하세요!")
+    println(100)
+
+    print("안녕")
+    print("하세요")
+    println()
+
+    print("> ")
+    var line = readLine() //키보드로 직접 입력하는 부분
+    println(line)
+}
